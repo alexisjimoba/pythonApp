d4bfb23687e7e34796d3c82d205c6771531c318d
Oscar Jiménez Obando <oscarjimoba@hotmail.com>
2025-09-12 01:20:18 -0600

    change  jenkins file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,14 @@
-# main.tf — solo providers
-variable "kubeconfig_path" {
-  type        = string
-  description = "Ruta al kubeconfig a usar por los providers"
-  default     = "~/.kube/config"
-}
-
+# Providers con TLS desactivado (insecure) y kubeconfig desde variable
 provider "kubernetes" {
-  config_path = pathexpand(var.kubeconfig_path)
+  load_config_file = true
+  config_path      = pathexpand(var.kubeconfig_path)
+  insecure         = var.kube_insecure
 }
 
 provider "helm" {
   kubernetes {
-    config_path = pathexpand(var.kubeconfig_path)
+    load_config_file = true
+    config_path      = pathexpand(var.kubeconfig_path)
+    insecure         = var.kube_insecure
   }
 }
