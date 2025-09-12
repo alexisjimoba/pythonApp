191ca339490883263c3f194a57c6de1d1e4a3a88
Oscar Jiménez Obando <oscarjimoba@hotmail.com>
2025-09-12 03:49:37 -0600

    change  jenkins file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,17 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+}
 
-
-# Opción simple (usa ~/.kube/config dentro del contenedor Jenkins)
+# Usa ~/.kube/config dentro del contenedor (o KUBECONFIG del entorno)
 provider "kubernetes" {}
 
-# --- Si prefieres ruta explícita, usa ESTA forma (no las dos) ---
-# provider "kubernetes" {
-#   config_path = pathexpand(var.kubeconfig_path)
-# }
+# Recurso mínimo para validar el plan
+resource "kubernetes_namespace" "pythonapp" {
+  metadata { name = "pythonapp" }
+}
