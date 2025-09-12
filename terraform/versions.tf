1a7559ad3597f59542e123640de69c66a11679f3
Oscar Jiménez Obando <oscarjimoba@hotmail.com>
2025-09-11 19:09:39 -0600

    change  jenkins file

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,7 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.30"
-    }
+    helm        = { source = "hashicorp/helm",        version = "~> 2.13" }
+    kubernetes  = { source = "hashicorp/kubernetes",  version = "~> 2.30" }
   }
 }
