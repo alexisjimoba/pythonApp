453d51ccf5ecf3ae765fc544ebea4762a4ecd551
Oscar Jiménez Obando <oscarjimoba@hotmail.com>
2025-09-11 19:44:07 -0600

    change  jenkins file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,16 @@
-############################################
-# main.tf (completo)
-############################################
-
-# Recibe la ruta al kubeconfig. En Jenkins la sobreescribimos con TF_VAR_kubeconfig_path
+# main.tf — solo providers
 variable "kubeconfig_path" {
-  description = "Ruta al kubeconfig; usa ~ para el home del usuario."
   type        = string
+  description = "Ruta al kubeconfig a usar por los providers"
   default     = "~/.kube/config"
 }
 
-locals {
-  kubeconfig_expanded = pathexpand(var.kubeconfig_path)
-}
-
 provider "kubernetes" {
-  config_path    = local.kubeconfig_expanded
-  config_context = "minikube"
+  config_path = pathexpand(var.kubeconfig_path)
 }
 
 provider "helm" {
   kubernetes {
-    config_path    = local.kubeconfig_expanded
-    config_context = "minikube"
+    config_path = pathexpand(var.kubeconfig_path)
   }
-}
-
-# Namespace donde se instalará la app
-resource "kubernetes_namespace" "ns" {
-  metadata { name = var.namespace }
-}
-
-# Salida útil
-output "namespace" {
-  value       = kubernetes_namespace.ns.metadata[0].name
-  description = "Namespace utilizado para el despliegue."
 }
